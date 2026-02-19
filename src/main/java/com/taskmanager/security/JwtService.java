56ad5155b79d13cd47bc03ac3d6ca296096fe120
Michael-merlot <124175319+Michael-merlot@users.noreply.github.com>
2026-02-19 17:25:42 +0300

    Add dtos and Security (JWTService)

---
+++ src/main/java/com/taskmanager/security/JwtService.java
@@ -0,0 +1,56 @@
+package com.taskmanager.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import javax.crypto.*;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret:mySecretKey}")
+    private String secretKey;
+
+    @Value("${jwt.expiration:86400000}")
+    private long jwtExpiration;
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(String email, Long userId){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpiration);
+
+        return Jwts.builder()
+                .subject(email)
+                .claim("userId", userId)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String getEmailFromToken(String token){
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException exception){
+            return false;
+        }
+    }
+}
